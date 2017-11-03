3962a69fc09e0cb28fbcf26c8caebbfbab2254ab
unknown <jack@iopcap.com>
2017-11-03 11:47:44 +0400

    ready to buy

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const restify = require('restify');
 const builder = require('botbuilder');
+const MongoClient = require('mongodb').MongoClient;
+const Exchanges = require('crypto-exchange')
+const _key          = '22022cff9b7442d6b2760bccf10edbfc'; // API Key
+const _secret       = '64bc002ecc6f497fab76018472eb3baf'; // API Private Key
 
 /*
 var mysql      = require('mysql');
@@ -15,6 +19,15 @@ setInterval(function () {
     connection.query('SELECT 1');
 }, 5000);
 */
+
+global.db=null; //database handle
+MongoClient.connect(process.env.mongoConnect||"mongodb://localhost:27017", function(err, database) {
+  if(!err) {
+    console.log("DB connected");
+	//Create a inventory
+	db = database;
+  } else console.log(err.stack);
+});
 
 //=========================================================
 // Bot Setup
@@ -85,10 +98,159 @@ bot.dialog('/start', [
     }
 ]).reloadAction('reloadMenu', null, { matches: /^start|show menu/i });
 
-
 bot.dialog('/buy', [
     function (session) {
+		session.userData.trade = "B"
+		checkBalance(session);
+	}		
+]).triggerAction({matches: /^(B|b)uy$/});
+
+function checkBalance(session) {
+	// find the balance for this user
+	db.collection('balances').findOne({userId: session.message.user.id, currency: "USD"}, function(err, user) {
+		if (err) console.log(err)
+		if (user) {
+			session.send('Your balance is: ' + user.balance+ ' USD')
+			session.userData.balance = user.balance
+			session.beginDialog('/trade')
+		}
+		else {
+			// give a new user a balnce of $10,000
+			db.collection('balances').insertOne({userId: session.message.address.user.id,
+				currency: "USD",
+				balance: 10000
+				})	
+			session.send('As a new user you have been given a balnce of: 10,000 USD')
+			session.userData.balance = 10000
+			session.beginDialog('/trade')
+			/*
+			session.send('You dont have enough balance available. Please make a deposit first')
+			session.beginDialog('/start')
+			*/
+		}
+	})
+	
+}
+
+bot.dialog('getPrice', [
+    function (session,args) {	
+		var msg = "At what price would you like to buy? Enter 0 for marketorder."
+		if (args && args.reprompt) {
+			if (args.reprompt == "neg") var msg = 'Price can not be negative. ' + msg
+			if (args.reprompt == "bal") var msg = 'Total order value exceeds your balance. ' + msg
+		}
+		builder.Prompts.text(session, msg);
+	},
+	function(session, results) {
+		if (results.response < 0) {
+			session.replaceDialog('getPrice', { reprompt: "neg" });
+		} else {
+			if (results.response == 0) 
+				session.userData.orderType = 'M'
+			else {
+				session.userData.orderType = 'L'
+				session.userData.price = results.response
+			}
+			session.replaceDialog('getvolume')
+			//builder.Prompts.number(session, "What volume?");
+			//session.endDialogWithResult({ response: results.response });
+		}
+	}	
+])
+
+bot.dialog('getvolume', [
+    function (session, args) {	
+		if (args && args.reprompt) {
+			if (args.reprompt == "neg") session.send('Amount has to be positive.')
+		}
+		builder.Prompts.text(session, 'What amount would you like to buy?');
+	},
+	function(session, results) {
+		session.userData.volume = results.response;
+		if (results.response <= 0) 
+			session.replaceDialog('getvolume', { reprompt: "neg" })
+		else {	
+			var totalOrderAmount = session.userData.volume * session.userData.price
+			// totalOrderAmount can not exceed balance
+			if (totalOrderAmount > session.userData.balance)
+				session.replaceDialog('getPrice', { reprompt: "bal" });
+			else	
+				session.endDialogWithResult({ response: results.response })
+		}
+	}	
+])
+
+bot.dialog('/trade', [
+    function (session) {	
 		builder.Prompts.text(session, "What crypto (BTC/ETH) would you like to buy?");
+	},
+	function(session, results) {
+		session.userData.currency = results.response;
+		var pair = results.response + '_USD'
+		Exchanges.kraken.ticker(pair)
+			.then(function (text) {
+				console.log(text)
+				for (var i in text) {
+					session.send('Price is currently: '+text[i].ask + ' USD')
+					session.userData.price = text[i].ask
+				}
+				session.beginDialog('getPrice');
+		})
+    }, 	
+	function(session, results) {
+		//var buyprice = session.userData.price ? session.userData.price : session.userData.marketprice
+		var confirm = 'Please confirm that you want to BUY '+session.userData.volume+' '+session.userData.currency+ ' at $'+session.userData.price +' Total: $'+ session.userData.volume * session.userData.price
+		builder.Prompts.confirm(session, confirm);
+    },	
+    function (session, results) {
+		if (results.response) var msg = 'Thank you. Your order has been placed'
+		else var msg = 'OK, Your order has been cancelled'
+		processOrder(session)
+        session.endConversation(msg);
+		session.beginDialog('/start')
+	}			
+])
+
+function processOrder(session) {
+
+	var buyprice = (session.userData.price ? session.userData.price : session.userData.marketprice)
+	var amount = session.userData.volume * buyprice
+	var vol = session.userData.volume *1
+
+	// process marketorders directly
+	if (session.userData.orderType == 'M') {
+		console.log('process market order')
+		db.collection('trades').insertOne({userId: session.message.address.user.id, 
+				userName: session.message.address.user.name, 
+				date:(new Date()).getTime(),
+				currency: session.userData.currency,
+				price: session.userData.price,
+				volume: vol,
+				amount: amount,
+				ordertype: session.userData.orderType
+				})	
+		//update balances
+		db.collection('balances').updateOne({userId:  session.message.address.user.id, currency: "USD"}, {$inc: {balance: -amount}});
+		db.collection('balances').update({userId:  session.message.address.user.id, currency: session.userData.currency}, {$inc: {balance: vol}}, { upsert: true });		
+	} else {
+		// process limit orders later
+		console.log('process limit order')
+		db.collection('orders').insertOne({userId: session.message.address.user.id, 
+			userName: session.message.address.user.name, 
+            date:(new Date()).getTime(),
+			currency: session.userData.currency,
+			price: session.userData.price,
+			volume: vol,
+            amount: amount,
+			ordertype: session.userData.orderType
+			})
+	}
+	
+}
+
+bot.dialog('/sell', [
+    function (session) {
+		builder.Prompts.text(session, "What crypto (BTC/ETH) would you like to sell?");
 	},
 	function(session, results) {
 		session.userData.crypto = results.response;
@@ -107,5 +269,29 @@ bot.dialog('/buy', [
         session.endConversation("You chose '%s'", results.response ? 'yes' : 'no');
 		session.beginDialog('/start')
 	}			
-]).triggerAction({matches: /^(B|b)uy$/});
+]).triggerAction({matches: /^(S|s)ell$/});
+
+bot.dialog('/db', [
+    function (session) {
+		builder.Prompts.text(session, "Database entry");             
+		db.collection('users').findOne({userId: session.message.user.id}, function(err, user) {
+			if (err) console.log(err)
+			console.log('User:%j', user)
+		})
+		db.collection('balances').find({userId: session.message.user.id}).toArray(function(err, balances) {
+			for (i in balances) {
+				console.log(balances[i].currency + ':'+balances[i].balance)
+			}
+		})
+		//update
+		//db.collection('balances').updateOne({userId: session.message.user.id, currency: "USDt"}, {$set: {balance: 6000}});
+		
+		//insert
+		db.collection('balances').insertOne({userId: session.message.user.id, currency: "USD", balance: 10000})
+	},
+    function (session, results) {
+        session.endConversation("You chose '%s'", results.response ? 'yes' : 'no');
+		session.beginDialog('/start')
+	}			
+]).triggerAction({matches: /^(D|d)b$/});
 
